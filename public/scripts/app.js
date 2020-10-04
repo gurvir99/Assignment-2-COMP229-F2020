292c012d6a18306c62914f04fb92d1acd278831f
gurvir99 <singh.gurvir37@gmail.com>
2020-10-04 19:36:22 -0400

    First Commit

---
+++ public/scripts/app.js
@@ -0,0 +1,10 @@
+//Iifee -- Immediately Invoked Function Expression
+(function(){
+
+function start(){
+    console.log("Your app has started successfully...")
+}
+
+window.addEventListener("load", start);
+
+})();
