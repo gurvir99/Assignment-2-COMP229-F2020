45d61b49c1bbe1374143366a9e1fc678b2cd993e
gurvir99 <singh.gurvir37@gmail.com>
2020-10-07 14:18:30 -0400

    Added form to Contact Page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,5 @@
+//const { event } = require("jquery");
+
 //Iifee -- Immediately Invoked Function Expression
 (function(){
 
@@ -7,4 +9,54 @@ function start(){
 
 window.addEventListener("load", start);
 
+if(document.title == "Contact Me")
+{
+    console.log("on contact page")
+    let sendButton = document.getElementById("sendButton");
+    let cancelButton = document.getElementById("resetButton");
+
+    sendButton.addEventListener("click", (event) => {
+       event.preventDefault();
+
+       let firstName = document.getElementById("fname").value;
+       let lastName = document.getElementById("lname").value;
+       let number = document.getElementById("tel").value;
+       let email = document.getElementById("email").value;
+       let message = document.getElementById("message").value;
+       let form = document.forms[0];
+
+       console.log("First Name: " + firstName);
+       console.log("Last Name: " + lastName);
+       console.log("Contact Number: " + number);
+       console.log("Email: " + email);
+       console.log("Message: " + message );
+       console.log("");
+
+       if(firstName =="" || lastName=="" || email=="" )
+       {
+           confirm("Please fill the form...")
+       }
+       else
+       {
+        if(confirm("Your submission was successfull..."))
+        {
+           location.href = "/home";
+        }
+       }
+
+       form.reset();
+       
+      
+    })
+
+    cancelButton.addEventListener("click", (event) => {
+       event.preventDefault();
+        if(confirm("Are you sure?"))
+        {
+           location.href = "/home";
+        }
+    })
+}
+
 })();
+
