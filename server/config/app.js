da93784beed7dc4d11f23a04781b8a11670b785a
gurvir99 <singh.gurvir37@gmail.com>
2020-10-19 15:43:34 -0400

    Installed authentication modules

--- server/config/app.js
+++ server/config/app.js
@@ -4,13 +4,19 @@
 //Date: October 09, 2020
 
 
-
 //installed third party packages
 let  createError = require('http-errors');
 let  express = require('express');
 let  path = require('path');
 let  cookieParser = require('cookie-parser');
 let  logger = require('morgan');
+
+//modules for authentication
+let session = require('express-session');
+let passport = require('passport');
+let passportLocal = require('passport-local');
+let localStrategy = passportLocal.Strategy;
+let flash = require('connect-flash');
 
 //database setup
 let mongoose = require('mongoose');
@@ -44,6 +50,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../node_modules')));
+
+//setup express session
+app.use(session({
+  secret: "SomeSecret",
+  saveUninitialized: false,
+  resave: false
+}));
+
+//initialize flash(shows error messages)
+app.use(flash());
+
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
